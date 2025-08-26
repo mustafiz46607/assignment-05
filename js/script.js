c0d87a1d97419b57193e14211494d6b6c095f7bc
mustafiz46607 <mustafizjr16@gmail.com>
2025-08-27 01:35:48 +0600

    heart button count funtionality done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-console.log("js file connected")
+const buttons = document.getElementsByClassName("heart-btn");
+const heartCount = document.getElementById("heart-count");
+let count = 0
+for (const button of buttons) {
+  button.addEventListener("click", function() {
+      count ++
+      heartCount.innerText = count
+  });
+}
