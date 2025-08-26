29d3b9817fea93f67d782e5a6517e2df0f31edb1
mustafiz46607 <mustafizjr16@gmail.com>
2025-08-27 03:56:32 +0600

    call buttons functionality done

--- js/script.js
+++ js/script.js
@@ -1,9 +1,34 @@
-const buttons = document.getElementsByClassName("heart-btn");
+//funtionality for heart btn clicked
+const heartButtons = document.getElementsByClassName("heart-btn");
 const heartCount = document.getElementById("heart-count");
 let count = 0
-for (const button of buttons) {
+for (const button of heartButtons) {
   button.addEventListener("click", function() {
       count ++
       heartCount.innerText = count
   });
 }
+
+//funtionality for call btn clicked
+const callBtns = document.getElementsByClassName("call-btn");
+const coinCount = document.getElementById("coin-count");
+let coins = 100;
+for (const btn of callBtns){
+    btn.addEventListener('click',function(){
+        const card = this.parentElement.parentElement
+        const serviceNames = card.getElementsByClassName("service-name");
+        const serviceNumbers = card.getElementsByClassName("service-number");
+        for(const service of serviceNames){
+            for(const number of serviceNumbers){
+                if(coins >= 20){
+                    coins -= 20
+                    coinCount.innerText = coins
+                    alert(`Calling ${service.innerText} ${number.innerText}... `);
+                }
+                else{
+                    alert("Insufficient coins for calling...")
+                }
+            }               
+        }  
+})
+}  
